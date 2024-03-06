382b34010a41f21784fb09142ee601412aeef99f
Hemant Agrawal <hemant.agrawal@workpulse.com>
2024-03-06 18:27:01 +0530

    committing the test cases

--- ApiAutomation/src/test/java/workpulseapi/AddAuditForm.java
+++ ApiAutomation/src/test/java/workpulseapi/AddAuditForm.java
@@ -5,6 +5,10 @@ import static io.restassured.http.ContentType.JSON;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Random;
 
 import org.json.*;
@@ -28,7 +32,7 @@ public class AddAuditForm extends ExtentReport{
 		String name = new Object(){}.getClass().getEnclosingMethod().getName();
 		extent = extentreport.createTest(name);
 		Random r = new Random();
-		int amount = r.nextInt(10,100);
+		int amount = r.nextInt(10,1000);
         String formname = "Form Created by Automation_" + amount;
         System.out.println("Form name is " +formname);
         String AccessToken = BearerToken.generateToken();
@@ -56,7 +60,8 @@ public class AddAuditForm extends ExtentReport{
 	     
 	    
 	      System.out.println("Form generated with id as: " + formid);  
-	      System.out.println("Form name is: " + formname);  
+	      System.out.println("Form name is: " + formname);
+	      extent.log(Status.INFO, "Form generated with name as: " + formname);
 	      extent.log(Status.INFO, "Form generated with id as: " + formid);
 	      
 	}
@@ -92,11 +97,91 @@ public class AddAuditForm extends ExtentReport{
 	    		  .statusCode(200)
 	    		  .log().all().extract().response();
 	      
+	      extent.log(Status.INFO, "Question added successfully to Audit");
+	      
+	}
+	
+	@Test(priority=4)
+	public static void assignlocation()throws IOException
+	{
+		String name = new Object(){}.getClass().getEnclosingMethod().getName();
+		extent = extentreport.createTest(name);
+		
+		String AccessToken = BearerToken.generateToken();
+		byte[] b = Files.readAllBytes(Paths.get("Models","AssignLocations.json"));
+		
+		
+	      String bdy = new String(b);
+	     JSONObject jobjectbdy= new JSONObject(bdy);
+	      JSONArray jarray = new JSONArray();
+	      jarray.put(jobjectbdy);
+	
+	      
+		 String strbdy = jarray.toString();
+
+	     
+	
+				
+	      
+	      Response response = RestAssured.given().contentType(JSON).
+	    		  header("Authorization", "Bearer " +AccessToken).body(strbdy).pathParam("formid", formid)
+	    		  .when().post("https://opsapi.workpulse.com/api/AuditForm/{formid}/locations")
+	    		  .then()
+	    		  .statusCode(200)
+	    		  .log().all().extract().response();
+	      extent.log(Status.INFO, "Location assigned successfully to Audit");
+	    
+	      
+	}
+	
+	@Test(priority=5)
+	public static void auditformsubmission()throws IOException
+	{
+		String name = new Object(){}.getClass().getEnclosingMethod().getName();
+		extent = extentreport.createTest(name);
+		
+		String AccessToken = BearerToken.generateToken();
+		byte[] b = Files.readAllBytes(Paths.get("Models","AuditFormSubmission.json"));
+		
+		
+	      String bdy = new String(b);
+	     JSONObject jobjectbdy= new JSONObject(bdy);
+	     jobjectbdy.put("FormId", formid);
+	  
+	    
+	    Date d1 = new Date();
+	    
+	    SimpleDateFormat dateformat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	    String currentdate = dateformat.format(d1);
+	    Calendar c = Calendar.getInstance();
+	    long timeinms= c.getTimeInMillis();
+	    Date afteraddingmins = new Date(timeinms- (5*60*1000));
+	    String starttime = dateformat.format(afteraddingmins);
+	   
+	    
+	     
+	 
+	     jobjectbdy.put("AuditStartTime", starttime);
+	     jobjectbdy.put("AuditEndTime", currentdate);
+	
+	    
+		 String strbdy = jobjectbdy.toString();
+
+	     
+	
+				
+	      
+	      Response response = RestAssured.given().contentType(JSON).
+	    		  header("Authorization", "Bearer " +AccessToken).body(strbdy)
+	    		  .when().post("https://opsapi.workpulse.com/api/audit/auditForm")
+	    		  .then()
+	    		  .statusCode(200)
+	    		  .log().all().extract().response();
+	      extent.log(Status.INFO, "Audit Form Submitted successfully");
 	    
 	      
 	}
 	
 	
 	}
-
 
--- ApiAutomation/src/test/java/workpulseapi/AddBookTask.java
+++ ApiAutomation/src/test/java/workpulseapi/AddBookTask.java
@@ -21,6 +21,9 @@ import io.restassured.response.Response;
 public class AddBookTask extends ExtentReport{
 	
 	public static String taskgeneratedname;
+	public static String taskid;
+	public static String groupid;
+	public static String groupdetailsid;
 	
 	@Test(priority=2)
 	public static void addbooktask()throws IOException
@@ -51,9 +54,78 @@ public class AddBookTask extends ExtentReport{
 	    		  .log().all().extract().response();
 	      JsonPath jsonpath = response.jsonPath();
 	      taskgeneratedname = jsonpath.getString("taskName");
+	      taskid = jsonpath.getString("id");
+	      groupid = jsonpath.getString("groups.id");
+	      groupdetailsid = jsonpath.getString("groups.details.id");
+	      
 	      System.out.println("Task generated with name as: " + taskgeneratedname);  
 	      extent.log(Status.INFO, "Task generated with name as: " + taskgeneratedname);
 	      
 	}
 
+	@Test(priority=3)
+	public static void addlocation()throws IOException
+	{
+		String name = new Object(){}.getClass().getEnclosingMethod().getName();
+		extent = extentreport.createTest(name);
+        String AccessToken = BearerToken.generateToken();
+		byte[] b = Files.readAllBytes(Paths.get("Models","AddBookTaskLocation.json"));
+		
+		
+	    String bdy = new String(b);
+	    
+	    JSONArray jarray = new JSONArray(bdy);
+
+	    String strbdy = jarray.toString();
+	
+				
+	      
+	      Response response = RestAssured.given().contentType(JSON).
+	    		  header("Authorization", "Bearer " +AccessToken).body(strbdy).
+	    		  pathParam("taskid", taskid)
+	    		  .when().post("https://opsapi.workpulse.com/api/booktask/{taskid}/locations")
+	    		  .then()
+	    		  .statusCode(200)
+	    		  .log().all().extract().response();
+	      
+	      extent.log(Status.INFO, "Location Assigned successfully to the Task");
+	      
+	}
+	
+	@Test(priority=4)
+	public static void updatebooktaskwithschedule()throws IOException
+	{
+		String name = new Object(){}.getClass().getEnclosingMethod().getName();
+		extent = extentreport.createTest(name);
+	
+      
+        String AccessToken = BearerToken.generateToken();
+		byte[] b = Files.readAllBytes(Paths.get("Models","UpdateBookTaskwithSchedule.json"));
+		
+		
+	      String bdy = new String(b);
+		  JSONObject json = new JSONObject(bdy);
+		  json.put("taskName", taskgeneratedname);
+		  json.put("groups.details.taskId", taskid);
+		  json.put("groups.id", groupid);
+		  json.put("groups.details.id", groupdetailsid);
+		  json.put("id",taskid);
+		  String strbdy = json.toString();
+
+	     
+	
+				
+	      
+	      Response response = RestAssured.given().contentType(JSON).
+	    		  header("Authorization", "Bearer " +AccessToken).body(strbdy)
+	    		  .when().post("https://opsapi.workpulse.com/api/booktask")
+	    		  .then()
+	    		  .statusCode(200)
+	    		  .log().all().extract().response();
+	      
+	    
+	}
+
+	
+	
 }
